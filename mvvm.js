1f39526325e376a8afffa0afd535078665329254
张敏 <zhrm1984@163.com>
2017-07-27 21:25:55 +0800

    添加计算属性

--- mvvm.js
+++ mvvm.js
@@ -1,18 +1,38 @@
 class Mvvm {
     constructor(options) {
         this.$options = options || {};
-        var data = this._data = this.$options.data;
+        var data = this._data = this.$options.data
+        var computedData = this.$options.computed
         var me = this;
         const keys = Object.keys(data)
         let i = keys.length
         while (i--) {
             proxy(me, keys[i])
         }
-        // observe data
+        this._initComputed(computedData)
+            // observe data
         observe(data)
-        this.$compile=new Compile(this.$options.el,this)
+        this.$compile = new Compile(this.$options.el, this)
+    }
+
+    _initComputed(computed) {
+        var me = this
+        if (computed != null && typeof computed === 'object') {
+            var keys = Object.keys(computed)
+            let i = keys.length
+            while (i--) {
+                let fn = computed[keys[i]]
+                Object.defineProperty(me, keys[i], {
+                    get() {
+                        return fn.bind(me)()
+                    }
+                })
+            }
+        }
     }
 }
+
+
 
 function proxy(ob, key) {
     Object.defineProperty(ob, key, {
@@ -25,6 +45,6 @@ function proxy(ob, key) {
     })
 }
 
-function observe(data){
-	new Observer(data)
+function observe(data) {
+    new Observer(data)
 }
